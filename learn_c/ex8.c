3fff05e88896077e3ca5df59370484500bab0a9f
Sam Rang <srang2010@gmail.com>
2015-02-15 17:05:11 -0600

    arrays and numbers

--- learn_c/ex8.c
+++ learn_c/ex8.c
@@ -11,12 +11,23 @@ int main(int argc, char* argv[])
   printf("The size of an int: %ld\n", sizeof(int));
   printf("The size of areas(int[]): %ld\n",
       sizeof(areas));
-  printf("The number of ints in areas: %ls\n",
+  printf("The number of ints in areas: %ld\n",
       sizeof(areas) / sizeof(int));
   printf("The first area is %d, the second %d.\n",
       areas[0], areas[1]);
+
   printf("The size f a char: %ld\n", sizeof(char));
+  printf("The size of name(char[]): %ld\n",
+      sizeof(name));
+  printf("The number of chars: %ld\n",
+      sizeof(name) / sizeof(char));
 
+  printf("The size of full_name(char[]): %ld\n",
+      sizeof(full_name));
+  printf("The number of chars: %ld\n",
+      sizeof(full_name) / sizeof(char));
 
+  printf("name=\"%s\" and full_name=\"%s\"\n",
+      name, full_name);
   return 0;
 }
