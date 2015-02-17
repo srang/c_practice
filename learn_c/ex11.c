1d5f0098beb188d806fddccdf03ae317a2a49383
Sam Rang <srang2010@gmail.com>
2015-02-16 19:20:15 -0600

    ex11

---
+++ learn_c/ex11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int i = 0;
+  while(i < argc) {
+    printf("arg %d: %s\n", i, argv[i++]);
+  }
+
+  char *states[] = {
+    "California", "Oregon", "Washington", "Texas"
+  };
+  int num_states = 4;
+  i = 0;
+  while(i < num_states) {
+    printf("state %d: %s\n", i, states[i++]);
+  }
+  return 0;
+}
