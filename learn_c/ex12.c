25c21710118aaadfb877d3066b447e50c33decdc
Sam Rang <srang2010@gmail.com>
2015-02-16 19:51:45 -0600

    if's and switches

---
+++ learn_c/ex12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  if(argc == 1) {
+    printf("You only have one argument sucker\n");
+  } else if(argc > 1 && argc < 4) {
+    printf("Here are your arguments:\n");
+    for(; i < argc; i++) {
+      printf("%s ", argv[i]);
+    }
+    printf("\n");
+  } else {
+    printf("You have too many arguments\n");
+  }
+  return 0;
+}
---
+++ learn_c/ex13.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+  if(argc != 2) {
+    printf("Error: you need exactly one argument\n");
+    return 1;
+  }
+  int i = 0;
+  char letter;
+  for(letter = argv[1][i]; argv[1][i] != '\0'; i++,letter = argv[1][i]) {
+    letter = (letter <= 90) ? letter + 32 : letter;
+    printf("%d:%c\n",letter,letter);
+    switch(letter) {
+      case 'a':
+      case 'A':
+        printf("%d: 'A'\n", i);
+        break;
+      case 'e':
+      case 'E':
+        printf("%d:'E'\n", i);
+        break;
+      case 'i':
+      case 'I':
+        printf("%d:'I'\n", i);
+        break;
+      case 'o':
+      case 'O':
+        printf("%d:'O'\n", i);
+        break;
+      case 'u':
+      case 'U':
+        printf("%d:'U'\n", i);
+        break;
+      case 'y':
+      case 'Y':
+        if(i > 2) {
+          printf("%d:'Y'\n", i);
+        }
+        break;
+      default:
+        printf("%d: %c is not a vowel\n", i, letter);
+    }
+  }
+  return 0;
+}
+      
